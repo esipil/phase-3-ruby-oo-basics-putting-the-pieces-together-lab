5cbc04dff66344aff14055175073d6c02747e291
Timothy <lemayiant4@gmail.com>
2022-10-21 18:19:56 +0300

    completed assignment

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,28 +1,14 @@
 # Make your shoe class here!
+require 'pry'
 class Shoe 
     attr_accessor :brand, :color, :size, :material, :condition
     def initialize (brand)
-        @brand
-    end
-    def brand 
-       "Nike"
-    end
-    def color
-        @color
-    end 
-    def size
-        @size
+        @brand = brand
     end
 
-    def material
-        @material
-    end
-
-    def condition
-        @condition
-    end
     def cobble 
         puts "Your shoe is as good as new!" 
         @condition = "new"
     end
 end
+
